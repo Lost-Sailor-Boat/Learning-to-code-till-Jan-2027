18ce6096e8974c412728f0d47d6d2366ed1467cd
Naman <nehakhubnani964@gmail.com>
2026-07-31 23:22:37 +0530

    Create Day4.py

---
+++ Day4.py
@@ -0,0 +1,11 @@
+Ran variables today
+like 
+A=1
+B=2
+print(A+B)
+This what i learn 
+Datatypes Integrers=1.2.3.4
+Floating Point Numbers=22.45.55.2
+Strings=Identifiers Means Name - Harry. Sonu 
+Booleans= True False
+None= Nothing Type Variable Didnt understand it properly but i will 
