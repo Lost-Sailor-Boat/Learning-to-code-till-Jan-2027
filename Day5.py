1209bfb3992451fd078813a7ae639ed40c0367f7
Naman <nehakhubnani964@gmail.com>
2026-08-03 22:18:17 +0530

    Create Day5.py

---
+++ Day5.py
@@ -0,0 +1,24 @@
+Opreators in Pythons 
+Airthmetic Operators 
+A=1
+B=2
+C=(A+B)
+print=(c)
+
+Assignment Operator 
+A=4-2
+B=3
+B /*-=6
+print(A)
+print(b)
+Compraison Oprator
+A=5!=5
+B=6<5
+C=5==5
+Will try in my project 
+Logcal Operator
+Truue is false 
+False is True 
+This is what i learnt Maybe correctly
+Not True = False
+Not False = True
