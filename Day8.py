925f349f912f13babe11036763f4a941496b9d93
Naman <nehakhubnani964@gmail.com>
2026-08-07 22:37:46 +0530

    Create Day8.py

---
+++ Day8.py
@@ -0,0 +1,7 @@
+Yesterday skipped and now i have learned the strings 
+single double and multicolon 
+slicing in strings 
+Index Starts and indext ends
+Name="Harry"
+0= is the very first no. in string
+Character1 Starts from the positive and character-1 from the negative way 
